b2b61fb750128de8a491a990b47aa569e62983d2
Feradicator <yadavshivam042001@gmail.com>
2024-08-11 05:15:31 +0530

    added message container

--- FrontEnd/chat-app/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ FrontEnd/chat-app/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MessageContainer = () => {
   return (
-    <div>
+    <div className='flex-1 overflow-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full'>
       message container
     </div>
   )
