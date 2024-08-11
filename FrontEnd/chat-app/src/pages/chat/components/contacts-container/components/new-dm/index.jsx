9783030bf2163e2ea5e150bff87db43ea5e06935
Feradicator <yadavshivam042001@gmail.com>
2024-08-11 16:27:48 +0530

    added search component

--- FrontEnd/chat-app/src/pages/chat/components/contacts-container/components/new-dm/index.jsx
+++ FrontEnd/chat-app/src/pages/chat/components/contacts-container/components/new-dm/index.jsx
@@ -4,6 +4,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { Input } from "../../../../../../components/ui/input";
 import { FaPlus } from "react-icons/fa";
 import {
   Dialog,
@@ -13,9 +14,18 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { animationDefaultOptions } from "@/lib/utils";
+import Lottie from "react-lottie";
+import { useState } from "react";
 
 const NewDm = () => {
   const [openNewContactModal, setopenNewContactModal] = useState(false);
+  const [searchedContacts, setsearchedContacts] = useState([])
+  const searchContacts=async(searchTerm)=>
+  {
+
+
+  }
   return (
     <>
       <TooltipProvider>
@@ -31,16 +41,36 @@ const NewDm = () => {
           </TooltipContent>
         </Tooltip>
       </TooltipProvider>
-      <Dialog>
-        <DialogTrigger>Open</DialogTrigger>
-        <DialogContent>
+      <Dialog open={openNewContactModal} onOpenChange={setopenNewContactModal}>
+
+        <DialogContent className='bg-[#181920] border-none text-white w-[400px] h-[400px] flex flex-col'>
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </DialogDescription>
+            <DialogTitle>Please Select Contact</DialogTitle>
+            <DialogDescription></DialogDescription>
           </DialogHeader>
+          <div>
+            <Input placeholder='Search Contatcs ' className='rounded-lg p-6 bg-[#2c2e3b] border-none'
+            onChange={(e)=>searchContacts(e.target.value)}/>
+          </div>
+          {
+            searchedContacts.length<=0 &&  
+            <div className="flex-1 md: bg-[#1c1d25] md:flex mt-5 flex-col   justify-center items-center  duration-1000 transition-all">
+            <Lottie
+              isClickToPauseDisabled={true}
+              height={100}
+              width={100}
+              options={animationDefaultOptions}
+            />
+      
+            <div className="text-opacity-80 text-white flex flex-col gap-5 items-center mt-5 lg:text-2xl text-xl transition-all duration-300 text-center">
+              <h3 className="poppins-medium">
+                Hi<span className="text-purple-500">!</span> Search New 
+                <span className="text-purple-500"> Contact. </span>Chat App
+                <span className="text-purple-500">.</span>
+              </h3>
+            </div>
+          </div>
+          }
         </DialogContent>
       </Dialog>
     </>
