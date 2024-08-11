c98692e045c9e9ac00001227a58a5f058cf0c37d
Feradicator <yadavshivam042001@gmail.com>
2024-08-11 15:37:48 +0530

    added dm component

---
+++ FrontEnd/chat-app/src/pages/chat/components/contacts-container/components/new-dm/index.jsx
@@ -0,0 +1,50 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { FaPlus } from "react-icons/fa";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+const NewDm = () => {
+  const [openNewContactModal, setopenNewContactModal] = useState(false);
+  return (
+    <>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <FaPlus
+              className="text-neutral-400 font-light text-opacity-90 text-start hover:text-neutral-100 cursor-pointer transition-all duration-300"
+              onClick={() => setopenNewContactModal(true)}
+            />
+          </TooltipTrigger>
+          <TooltipContent className="bg-[#1c1b1e] border-none mb-2 p-3 text-white">
+            Select New Contact
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default NewDm;
